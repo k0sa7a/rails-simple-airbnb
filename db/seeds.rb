b1e8abc5d07af50faa963c1a44ac0eb064c9ef79
Stefan Filev <stefan.s.filev@gmail.com>
2022-04-28 20:36:36 +0100

    added a seed file and created the view for index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing DB...'
+Flat.destroy_all
+
+puts 'Generating new DB with 3 flats...'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Big two bed flat in Shoreditch',
+  address: '10 Shoreditch High Street London Se 5GT',
+  description: 'The flat is perfect for parties and close to all the cool clubs in Shoreditch',
+  price_per_night: 190,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Cosy one bed flat in Camden',
+  address: '10 Camden High Street London NW9 1Tr',
+  description: 'Small flat for 2 people that was renovated very recently. Perfect for the busy professional',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+puts 'Done!'
